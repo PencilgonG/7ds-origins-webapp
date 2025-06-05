5e04fa191c679915073e4b7c94a4d64c6436fd34
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:24:48 +0200

    Create MobsPage.jsx

---
+++ src/pages/MobsPage.jsx
@@ -0,0 +1,8 @@
+const MobsPage = () => (
+  <div className="bg-black min-h-screen text-white p-6">
+    <h1 className="text-4xl font-bold mb-4">Boss & Mobs</h1>
+    <p>⚠️ Module en cours de développement</p>
+  </div>
+);
+
+export default MobsPage;
