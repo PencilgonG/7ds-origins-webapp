a71e3cb6a2b2f6efd4efb295f161601d45c0d165
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:22:57 +0200

    Create CharactersPage.jsx

---
+++ src/pages/CharactersPage.jsx
@@ -0,0 +1,8 @@
+const CharactersPage = () => (
+  <div className="bg-black min-h-screen text-white p-6">
+    <h1 className="text-4xl font-bold mb-4">Personnages Jouables</h1>
+    <p>⚠️ Module en cours de développement</p>
+  </div>
+);
+
+export default CharactersPage;
