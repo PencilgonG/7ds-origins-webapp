9010e6d353774f19ba1273503b4f847158a3e7d9
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:40:28 +0200

    Update EquipementsPage.jsx

--- src/pages/EquipementsPage.jsx
+++ src/pages/EquipementsPage.jsx
@@ -1,7 +1,14 @@
+import equipements from "../data/equipements.json";
+import EquipementCard from "../components/equipements/EquipementCard";
+
 const EquipementsPage = () => (
   <div className="bg-black min-h-screen text-white p-6">
-    <h1 className="text-4xl font-bold mb-4">Équipements & Artefacts</h1>
-    <p>⚠️ Module en cours de développement</p>
+    <h1 className="text-4xl font-bold mb-6">Équipements & Artefacts</h1>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+      {equipements.map((item, i) => (
+        <EquipementCard key={i} item={item} />
+      ))}
+    </div>
   </div>
 );
 
