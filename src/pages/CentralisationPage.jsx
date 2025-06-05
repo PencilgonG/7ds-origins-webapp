29386284e49f82af0a0e085ac7fda8db834c8a5b
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:41:24 +0200

    Update CentralisationPage.jsx

--- src/pages/CentralisationPage.jsx
+++ src/pages/CentralisationPage.jsx
@@ -1,7 +1,10 @@
+import data from "../data/centralisation.json";
+import CentralTable from "../components/centralisation/CentralTable";
+
 const CentralisationPage = () => (
   <div className="bg-black min-h-screen text-white p-6">
-    <h1 className="text-4xl font-bold mb-4">Centralisation de Données</h1>
-    <p>⚠️ Module en cours de développement</p>
+    <h1 className="text-4xl font-bold mb-6">Centralisation de Données</h1>
+    <CentralTable data={data} />
   </div>
 );
 
