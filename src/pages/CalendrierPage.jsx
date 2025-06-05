3df5d0f70193038d70d556509bc926f67f4e3d4d
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:40:01 +0200

    Update CalendrierPage.jsx

--- src/pages/CalendrierPage.jsx
+++ src/pages/CalendrierPage.jsx
@@ -1,8 +1,15 @@
-const CalendrierPage = () => (
+import characters from "../data/characters.json";
+import CharacterCard from "../components/personnages/CharacterCard";
+
+const CharactersPage = () => (
   <div className="bg-black min-h-screen text-white p-6">
-    <h1 className="text-4xl font-bold mb-4">Calendrier Joueurs / Raids</h1>
-    <p>⚠️ Module en cours de développement</p>
+    <h1 className="text-4xl font-bold mb-6">Personnages Jouables</h1>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      {characters.map((char, i) => (
+        <CharacterCard key={i} character={char} />
+      ))}
+    </div>
   </div>
 );
 
-export default CalendrierPage;
+export default CharactersPage;
