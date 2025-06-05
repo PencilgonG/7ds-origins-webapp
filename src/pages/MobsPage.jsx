c9e9afef76f127d0fec189ea77bcf5628a2aa61c
PencilgonG <unrankedtosomething@gmail.com>
2025-06-05 21:40:14 +0200

    Update MobsPage.jsx

--- src/pages/MobsPage.jsx
+++ src/pages/MobsPage.jsx
@@ -1,7 +1,14 @@
+import mobs from "../data/mobs.json";
+import MobCard from "../components/mobs/MobCard";
+
 const MobsPage = () => (
   <div className="bg-black min-h-screen text-white p-6">
-    <h1 className="text-4xl font-bold mb-4">Boss & Mobs</h1>
-    <p>⚠️ Module en cours de développement</p>
+    <h1 className="text-4xl font-bold mb-6">Boss & Mobs</h1>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
+      {mobs.map((mob, i) => (
+        <MobCard key={i} mob={mob} />
+      ))}
+    </div>
   </div>
 );
 
